1afae6332a666c5d2e8c6afb7c33ad88bb845b05
ewelina <ewelina.jesionowska@gmail.com>
2024-06-17 19:56:00 +0200

    changes in settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#BASE_DIR = C:\Users\eweli\PycharmProjects\habitpy.me
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -115,7 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+
+# Ścieżki do plików statycznych
+
+STATICFILES_DIRS = [
+    "config/static"
+]
+
+# Katalog, do którego będą kopiowane pliki statyczne podczas używania collectstatic
+STATIC_ROOT = BASE_DIR /'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
