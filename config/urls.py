9e9e5f85be51abd9d0b41e8961099268d090afcd
ewelina <ewelina.jesionowska@gmail.com>
2024-06-22 12:57:53 +0200

    changes in urls.py - restarting passwords

--- config/urls.py
+++ config/urls.py
@@ -34,6 +34,14 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('', views.home, name='home'),
     path('autocomplete/', views.autocomplete, name='autocomplete'),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+ #   path('accounts/', include('config.urls')),
 ]
 
 
